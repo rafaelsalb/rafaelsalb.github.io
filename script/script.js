963c47b5dae41c12f51f3ba967e768401f85b963
Rafael Albuquerque <61889057+rafaelsalb@users.noreply.github.com>
2022-06-19 20:54:06 -0300

    paginas

--- script/script.js
+++ script/script.js
@@ -2,3 +2,13 @@ function redirecionar(url)
 {
     window.location.href = url;
 }
+
+function app_web()
+{
+    alert("Projeto em desenvolvimento. Ainda não disponível ao público.");
+}
+
+function jogo()
+{
+    alert('Link ainda não disponibilizado. Em breve estará funcionando.');
+}
